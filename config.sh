c4cd19647c4502b7d317f6404ebe64fdce6c622b
Jeff Vestal <53237856+jeffvestal@users.noreply.github.com>
2023-09-27 10:21:59 -0500

    adding cohere script and config

---
+++ config.sh
@@ -0,0 +1,7 @@
+export ES_EMBEDDING_MODEL_ID="<Elasticsearch Embedding Model"
+export ES_CLOUD_ID="<Elastic Cloud Id>"
+export ES_INDEX="<Elastic Index Name>"
+export ES_USERNAME="<Elastic User Name>"
+export ES_PASSWORD="<Elastic Password>"
+export COHERE_LIGHT_ENDPOINT="<LLM Endpoint Name>"
+export AWS_REGION="us-east-1"
